57f2fa70fd6213fd216498e40fee858f15faa6cf
tyler <tystanish@gmail.com>
2019-10-11 18:21:17 -0500

    basic migrate.py functionality

---
+++ server/db/migrations/v1/up.sql
@@ -0,0 +1,4 @@
+create table user (
+    id serial primary key,
+    email text not null
+);
--- server/migrate.py
+++ server/migrate.py
@@ -11,7 +11,11 @@ def replace_sql_imports(sql: str) -> str:
     """
     Replaces instances of \i with the text from that file.
     Doing this to avoid having a separate migration for each
-    of the functions
+    of the functions.
+
+    Most postgresql drivers don't support
+    metacommands such as \i so we have to implement this functionality
+    ourselves
     """
     lines = []
     for line in sql.splitlines():
@@ -22,17 +26,18 @@ def replace_sql_imports(sql: str) -> str:
         lines.append(line)
     return '\n'.join(lines)
 
-def migrate(up_or_down, v_start, v_end):
+def migrate_str(up_or_down: str, v_start: int, v_end: int) -> str:
     """
     :param v_start: The starting version
     :param v_end: The ending version (exclusive)
     """
-    conn = psycopg2(**get_db_info())
-    cur = conn.cursor()
-    sql = ''
-    get_file_contents('db/migrations/')
+    sql = []
+    for version in range(v_start, v_end):
+        sql.append(replace_sql_imports(get_file_contents(f'db/migrations/v{version}/{up_or_down}.sql')))
+    return '\n'.join(sql)
 
 
 if __name__ == '__main__':
-    mirgrate(*sys.argv)
+    up_down, v_start, v_end = sys.argv[1:]
+    print(migrate_str(up_down, int(v_start), int(v_end)))
 
