b89e89f8e26baea266aa7124a35dfee695a0c924
tyler <tystanish@gmail.com>
2019-10-20 19:38:31 -0500

    added create_app from main and more testing setup

---
+++ server/main.py
@@ -0,0 +1,32 @@
+from flask import Flask, g
+
+import psycopg2.extras
+from psycopg2.pool import ThreadedConnectionPool
+
+from utils.db import get_db_info
+
+
+
+def create_app():
+    app = Flask(__name__)
+
+    db_info = get_db_info()
+    pool = ThreadedConnectionPool(0, 16, **db_info, cursor_factory=psycopg2.extras.RealDictCursor)
+
+    @app.before_request
+    def before_request():
+        g.db = pool.getconn()
+
+    @app.after_request
+    def after(res):
+        pool.putconn(g.db)
+        return res
+
+    # register blueprints
+    return app
+
+
+if __name__ == '__main__':
+    app = create_app()
+    app.run()
+
--- server/tests/utils.py
+++ server/tests/utils.py
@@ -1,30 +1,40 @@
 from unittest import TestCase
 import os
-
+os.environ['FLASK_ENV'] = 'testing'
 import psycopg2
 
+from main import create_app
 from migrate import migrate_str, get_latest_migration_version
 from utils.db import get_db_info
 
 
-class DbTest(TestCase):
+class FlaskIntegrationTest(TestCase):
     @classmethod
-    def setUpClass(self):
-        os.environ['FLASK_ENV'] = 'testing'
-        self.conn = psycopg2.connect(**get_db_info())
+    def setUpClass(cls):
+        print('hi from FlaskIntegrationTest!')
+        cls.app = create_app()
+
+
+class DbTest(FlaskIntegrationTest):
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        print('hi from DbTest!')
+        cls.conn = psycopg2.connect(**get_db_info())
         # run migrations, detect current migration by default
         sql = migrate_str('up', 1, get_latest_migration_version())
-        cur = self.conn.cursor()
+        cur = cls.conn.cursor()
         cur.execute(sql)
-        self.conn.commit()
+        cls.conn.commit()
         cur.close()
 
     @classmethod
-    def tearDownClass(self):
+    def tearDownClass(cls):
+        print('bye from DbTest!')
         sql = migrate_str('down', get_latest_migration_version(), 1)
-        cur = self.conn.cursor()
+        cur = cls.conn.cursor()
         cur.execute(sql)
-        self.conn.commit()
+        cls.conn.commit()
         cur.close()
-        self.conn.close()
+        cls.conn.close()
 
