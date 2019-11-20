540c0b292f014cc8cf3efe197735020653224f9f
tyler <tystanish@gmail.com>
2019-11-20 13:53:44 -0600

    added wsgi and moved everything in server/ to the top level

--- utils/db.py
+++ utils/db.py
@@ -16,6 +16,14 @@ def get_db_info() -> dict:
     Gets the database info depending on FLASK_ENV
     """
     env = os.environ['FLASK_ENV']
+    if env == Environments.PRODUCTION.value:
+        return {
+            'dbname': os.environ['DBNAME'],
+            'user': os.environ['DBUSER'],
+            'host': os.environ['DBUSER'],
+            'password': os.environ['DBPASSWORD'],
+            'port': os.environ['DBPORT'],
+        }
     joined = os.path.join(fs.get_absolute_path(), '../')
     # TODO please add condition checks for other environments
     return json.loads(fs.get_file_contents(joined + f'secrets/{env}.db.json'))
