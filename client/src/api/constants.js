52237a29de28281a03876435b8dec7f3eb0ebecf
tyler <tystanish@gmail.com>
2019-11-21 11:18:42 -0600

    using env var now for api url, trying different static hosting

--- client/src/api/constants.js
+++ client/src/api/constants.js
@@ -1,5 +1,6 @@
-const BASE_URL = '/api/'
+// export const BASE_URL = '/api/'
 // export const BASE_URL = 'http://localhost:5000/api/'
+export const BASE_URL = process.env.REACT_APP_API_URL
 export const LOGIN_URL = BASE_URL + 'auth/login/'
 export const SIGNUP_URL = BASE_URL + 'auth/signup/'
 export const EVENT_URL = BASE_URL + 'event/'
