f29cc53b886af4a7ce7057f7138f0aaa14596ea3
tyler <tystanish@gmail.com>
2019-11-22 21:11:54 -0600

    added labels to start and end date/time and added form validation too

--- client/src/components/modals/CreateEvent.js
+++ client/src/components/modals/CreateEvent.js
@@ -21,16 +21,26 @@ const CreateEvent = props => {
           placeholder='Title'
           className='login-form-item'
         />
+        <h3>Start date and time</h3>
         <DateTimePicker
           onChange={date => setEventStart(date)}
           value={eventStart}
         />
+        <h3>End date and time</h3>
         <DateTimePicker
           onChange={date => setEventEnd(date)}
           value={eventEnd}
         />
-        <Button text='Create Event' fill onClick={e => {
+        <Button style={{marginTop: '15px'}} text='Create Event' fill onClick={e => {
           e.preventDefault()
+          if (!title || !eventStart || !eventEnd) {
+            alert('Please insert valid title, event start and end!')
+            return
+          }
+          if (eventEnd <= eventStart) {
+            alert('The end must come after the start!')
+            return
+          }
           props.createEvent(title, eventStart, eventEnd)
           props.close()
         }} type='submit'/>
